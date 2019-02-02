6e9a09bdcd5703a8d793155463e2587efc25ca1b
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2019-02-02 15:30:00 -0500

    created contact model

--- app.js
+++ app.js
@@ -4,7 +4,22 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
+// database setup
+let mongoose = require('mongoose');
+let DB = require('./config/db');
+
+// point Mongoose to the DB URI
+mongoose.connect(DB.URI);
+
+let mongoDB = mongoose.connection;
+mongoDB.on('error', console.error.bind(console, 'Connection Error:'));
+mongoDB.once('open', ()=> {
+  console.log("Connected to MongoDB...");
+});
+
 let indexRouter = require('./routes/index');
+let contactRouter = require('./routes/contact');
+
 
 let app = express();
 
@@ -20,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', indexRouter);
+app.use('/contact-list', contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    "URI" : "mongodb://localhost/test"
+}
