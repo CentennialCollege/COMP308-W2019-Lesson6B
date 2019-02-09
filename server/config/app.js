a182ff301651a02c24875e367ca0948547eb424c
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2019-02-09 17:05:23 -0500

    reorganized project structure

--- server/config/app.js
+++ server/config/app.js
@@ -6,7 +6,7 @@ let logger = require('morgan');
 
 // database setup
 let mongoose = require('mongoose');
-let DB = require('./config/db');
+let DB = require('./db');
 
 // point Mongoose to the DB URI
 mongoose.connect(DB.URI);
@@ -17,22 +17,22 @@ mongoDB.once('open', ()=> {
   console.log("Connected to MongoDB...");
 });
 
-let indexRouter = require('./routes/index');
-let contactRouter = require('./routes/contact');
+let indexRouter = require('../routes/index');
+let contactRouter = require('../routes/contact');
 
 
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(path.join(__dirname, '../../node_modules')));
 
 app.use('/', indexRouter);
 app.use('/contact-list', contactRouter);
