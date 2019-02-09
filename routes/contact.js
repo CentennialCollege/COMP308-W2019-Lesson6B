018b1204a0c6939ca3429516e499ec0b70ef0e0a
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2019-02-09 15:35:17 -0500

    added Update functionality to the DB

--- routes/contact.js
+++ routes/contact.js
@@ -52,4 +52,47 @@ router.post('/add', (req, res, next) => {
     });
 });
 
+/* GET request - display the Edit page */
+router.get('/edit/:id', (req, res, next) => {
+    let id = req.params.id;
+
+    contactModel.findById(id, (err, contactObject) => {
+        if(err) {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            // show the edit view
+            res.render('contacts/edit', {
+                title: 'Edit Contact',
+                contact: contactObject
+            });
+        }
+    });
+});
+
+/* POST request - Update the database with data from the Edit Page */
+router.post('/edit/:id', (req, res, next) => {
+    let id = req.params.id;
+
+    let updatedContact = contactModel({
+        "_id": id,
+        "firstName": req.body.firstName,
+        "lastName": req.body.lastName,
+        "age": req.body.age
+    });
+
+    contactModel.update({_id: id}, updatedContact, (err) => {
+        if(err) {
+            console.log(err);
+            res.end(err);
+        }
+        else {
+            // refresh the contact list
+            res.redirect('/contact-list');
+        }
+    })
+});
+
 module.exports = router;
